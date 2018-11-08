d434af88851987c276cd4ae2f66d02bd22e75ce1
Mariano Lopez <m_villa@hotmail.com>
2018-11-08 08:33:05 -0300

    new

--- PairsFromArrayGivenSum/src/main/kotlin/com/z/Main.kt
+++ PairsFromArrayGivenSum/src/main/kotlin/com/z/Main.kt
@@ -1,30 +1,67 @@
 package com.z
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import java.io.File
-import java.util.*
+import com.fasterxml.jackson.core.JsonFactory
+import com.fasterxml.jackson.core.JsonToken
+import java.io.BufferedInputStream
+import java.io.FileInputStream
 import kotlin.system.measureTimeMillis
 
 
-val pairs: MutableList<String> = Collections.synchronizedList(mutableListOf<String>())
+val foundPairs: MutableList<Pair<Int,Int>> = mutableListOf()
+val inMemoryIndex:MutableMap<Int,Int> = mutableMapOf()
+private var givenSum: Int = 0
+private var arraySize:Int = 0
+
 
 fun main(args: Array<String>) {
     val time = measureTimeMillis{
-        val (array,sum) = getInputStream(args[0])
-        println("array size: ${array.size} - sum: $sum")
-        process(array, sum)
-        println("pairs size: ${pairs.size} -> $pairs")
+        withCustomParser(args.first())
     }
-    println("Completed in $time ms")
+    println("CustomParser Completed in $time ms")
+}
+private fun withCustomParser(path:String){
+    val factory = JsonFactory()
+    factory.createParser(BufferedInputStream(FileInputStream(path))).use {
+        while(it.nextToken() != JsonToken.END_OBJECT){
+            when(it.currentName){
+                "sum" -> {
+                    it.nextToken()
+                    givenSum = it.intValue
+                }
+                "array"->{
+                    it.nextToken()
+                    while(it.nextToken()!=JsonToken.END_ARRAY){
+                        arraySize++
+                        val value = it.valueAsInt
+                        if(!inMemoryIndex.containsKey(value)|| givenSum/2==value){
+                            if(inMemoryIndex.containsKey(givenSum-value)){
+                                (givenSum-value to value).apply { if(!foundPairs.contains(this))foundPairs.add(this) }
+                            }else{
+                                inMemoryIndex[value] = value
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    println("array size: $arraySize - givenSum: $givenSum")
+    println("foundPairs size: ${foundPairs.size} -> $foundPairs")
+}
+
+
+/*private fun withoutParser(path: String){
+    val (array,sum) = getInputStream(path)
+    println("array size: ${array.size} - givenSum: $sum")
+    process(array, sum)
+    println("foundPairs size: ${foundPairs.size} -> $foundPairs")
 }
 
 private fun process(array:List<Int>, sum:Int){
-    val map:MutableMap<Int,Int> = Collections.synchronizedMap(mutableMapOf())
-    array.distinct().stream().parallel().forEach {
+    val map:MutableMap<Int,Int> = mutableMapOf()
+    array.forEach {
         if(map.containsKey(sum-it)){
-            pairs.add("(${sum-it},$it)")
+            foundPairs.add(givenSum-it to it)
         }else{
             map[it] = it
         }
@@ -33,4 +70,4 @@ private fun process(array:List<Int>, sum:Int){
 
 private fun getInputStream(path:String) = jacksonObjectMapper().apply { configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true) }.readValue<Input>(File(path))
 
-data class Input(val array:List<Int>, val sum:Int)
+data class Input(val array:List<Int>, val sum:Int)*/
